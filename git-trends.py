dcb0bcd9af1f9f8f5d431d0681cd924f8861d4fd
Harsh Vardhan <harsh59v@gmail.com>
2017-05-09 16:05:19 +0530

    Adds fetch capabilities using Github API
    ALERT: Uses experimental API

---
+++ git-trends.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python3
+import requests
+import datetime
+
+def askGithub(dateCreated, lang, sortOrder = True):
+    payload = { 'q' : ['language:' + lang,
+                'created:>' + str(dateCreated)],
+		'sort': 'stars',
+		'order': 'desc' if sortOrder else 'inc',
+              }
+    head = { 'Accept' : 'application/vnd.github.mercy-preview+json' }
+    x = requests.get('https://api.github.com/search/repositories', params = payload, headers = head)
+    print(x.content)
+
+if __name__ == '__main__':
+    askGithub(dateCreated = datetime.date.today(), lang = 'go')
+	
