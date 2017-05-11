164e5f146d8a0240fbac81f4fab82ca50c9c1d25
Harsh Vardhan <harsh59v@gmail.com>
2017-05-11 10:11:05 +0530

    Fix payload's order attribute value

--- git-trends.py
+++ git-trends.py
@@ -6,12 +6,11 @@ def askGithub(dateCreated, lang, sortOrder = True):
     payload = { 'q' : ['language:' + lang,
                 'created:>' + str(dateCreated)],
 		'sort': 'stars',
-		'order': 'desc' if sortOrder else 'inc',
+		'order': 'desc' if sortOrder else 'asc',
               }
     head = { 'Accept' : 'application/vnd.github.mercy-preview+json' }
     x = requests.get('https://api.github.com/search/repositories', params = payload, headers = head)
     print(x.content)
 
 if __name__ == '__main__':
-    askGithub(dateCreated = datetime.date.today(), lang = 'go')
-	
+    askGithub(datetime.date.today(), 'go')
